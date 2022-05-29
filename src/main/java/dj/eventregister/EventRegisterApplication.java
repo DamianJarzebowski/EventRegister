204df4ab97f1b9f96a41c939b330f8f2e8ee8b56
DamianJarzebowski <damian.r.hajduk@o2.pl>
2022-05-29 02:43:26 +0200

    Initial commit

---
+++ src/main/java/dj/eventregister/EventRegisterApplication.java
@@ -0,0 +1,13 @@
+package dj.eventregister;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventRegisterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventRegisterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dj/eventregister/EventRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package dj.eventregister;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventRegisterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
