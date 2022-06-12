f5b7dd184b41400512f2ec9ecc44e6e6e141b0e7
DamianJarzebowski <damian.r.hajduk@o2.pl>
2022-06-13 00:37:56 +0200

    WIP

--- src/main/java/dj/eventregister/EventRegisterApplication.java
+++ src/main/java/dj/eventregister/EventRegisterApplication.java
@@ -2,7 +2,7 @@ package dj.eventregister;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.converter.json.GsonBuilderUtils;
+
 
 @SpringBootApplication
 public class EventRegisterApplication {
@@ -10,9 +10,7 @@ public class EventRegisterApplication {
     public static void main(String[] args) {
         SpringApplication.run(EventRegisterApplication.class, args);
 
-        System.out.println("d");
+
     }
-
-
 
 }
