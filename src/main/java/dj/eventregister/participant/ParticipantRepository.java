8a4eea7b7614ca29e4defa8bb7b69ed5066f48b9
DamianJarzebowski <damian.r.hajduk@o2.pl>
2022-06-13 16:48:31 +0200

    WIP

---
+++ src/main/java/dj/eventregister/participant/ParticipantRepository.java
@@ -0,0 +1,6 @@
+package dj.eventregister.participant;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
+}
---
+++ src/main/java/dj/eventregister/party/PartyRepository.java
@@ -0,0 +1,6 @@
+package dj.eventregister.party;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PartyRepository extends JpaRepository<Party, Long> {
+}
