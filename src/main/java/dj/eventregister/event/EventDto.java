b19e022d9cfb729a4022c7f8c46881e1161776bd
DamianJarzebowski <damian.r.hajduk@o2.pl>
2022-06-14 18:17:06 +0200

    WIP

---
+++ src/main/java/dj/eventregister/event/EventDto.java
@@ -0,0 +1,21 @@
+package dj.eventregister.event;
+
+import dj.eventregister.category.Category;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+public class EventDto {
+
+    private Long id;
+    private String name;
+    private String description;
+    private int maxParticipant;
+    private int minParticipant;
+    private boolean majority;
+    private LocalDate date;
+    private LocalTime time;
+    private Category category;
+}
---
+++ src/main/java/dj/eventregister/event/EventMapper.java
@@ -0,0 +1,47 @@
+package dj.eventregister.event;
+
+import dj.eventregister.category.Category;
+import dj.eventregister.category.CategoryRepository;
+
+import java.util.Optional;
+
+public class EventMapper {
+
+    private final CategoryRepository categoryRepository;
+
+    public EventMapper(CategoryRepository categoryRepository) {
+        this.categoryRepository = categoryRepository;
+    }
+
+    EventDto toDto(Event event) {
+        var dto = new EventDto();
+
+        dto.setId(event.getId());
+        dto.setName(event.getName());
+        dto.setDescription(event.getDescription());
+        dto.setMaxParticipant(event.getMaxParticipant());
+        dto.setMinParticipant(event.getMinParticipant());
+        dto.setMajority(event.isMajority());
+        dto.setDate(event.getDate());
+        dto.setTime(event.getTime());
+        Optional<Category> category = categoryRepository.findByName(event.getName());
+        category.ifPresent(dto::setCategory);
+        return dto;
+    }
+
+    Event toEntity(EventDto eventDto) {
+        var entity = new Event();
+
+        entity.setId(eventDto.getId());
+        entity.setName(eventDto.getName());
+        entity.setDescription(eventDto.getDescription());
+        entity.setMaxParticipant(eventDto.getMaxParticipant());
+        entity.setMinParticipant(eventDto.getMinParticipant());
+        entity.setMajority(eventDto.isMajority());
+        entity.setDate(eventDto.getDate());
+        entity.setTime(eventDto.getTime());
+        Optional<Category> category = categoryRepository.findByName(eventDto.getName());
+        category.ifPresent(entity::setCategory);
+        return entity;
+    }
+}
