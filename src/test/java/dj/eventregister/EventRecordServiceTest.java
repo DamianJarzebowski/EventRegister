649cadd07b412d3fd8ef11831b4658c7c0a7e727
DamianJarzebowski <damian.r.hajduk@o2.pl>
2022-07-06 20:55:28 +0200

    WIP

---
+++ src/test/java/dj/eventregister/EventRecordServiceTest.java
@@ -0,0 +1,17 @@
+package dj.eventregister;
+
+import dj.eventregister.eventrecord.EventRecordService;
+import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+
+@RequiredArgsConstructor
+public class EventRecordServiceTest {
+
+
+    @Test
+    void shouldCheckParticipantMajority() {
+
+        var sut = new EventRecordService();
+    }
+
+}
