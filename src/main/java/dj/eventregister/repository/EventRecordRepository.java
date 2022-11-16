9a020bf520285fce565f47f94803286342cea3e4
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2022-11-16 01:07:20 +0100

    clean code

--- src/main/java/dj/eventregister/repository/EventRecordRepository.java
+++ src/main/java/dj/eventregister/repository/EventRecordRepository.java
@@ -1,6 +1,7 @@
 package dj.eventregister.repository;
 
 import dj.eventregister.models.eventrecord.EventRecord;
+import dj.eventregister.models.participant.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
