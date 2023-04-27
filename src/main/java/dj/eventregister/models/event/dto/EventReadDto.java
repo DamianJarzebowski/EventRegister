33e19058b77eab329cae44dcc4afb3d5f0f56075
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2023-04-27 23:47:31 +0200

    clean EventReadDto

--- src/main/java/dj/eventregister/models/event/dto/EventReadDto.java
+++ src/main/java/dj/eventregister/models/event/dto/EventReadDto.java
@@ -6,19 +6,25 @@ import lombok.experimental.Accessors;
 
 import java.time.LocalDateTime;
 
-// Used to read full model event via API
-
 @Data
 @Accessors(chain = true)
 public class EventReadDto {
 
     private Long id;
+
     private String name;
+
     private String description;
+
     private int maxParticipant;
+
     private int minParticipant;
+
     private boolean majority;
+
     private LocalDateTime dateTime;
+
     private String category;
+
     private Event.EventStateMachine stateEvent;
 }
