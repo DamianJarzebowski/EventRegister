ebefd5663ab3b09bf3eb1c08ca16d0733d8481d2
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2023-05-06 21:09:51 +0200

    clean code

--- src/main/java/dj/eventregister/models/category/DuplicateCategoryNameException.java
+++ src/main/java/dj/eventregister/models/category/DuplicateCategoryNameException.java
@@ -3,6 +3,6 @@ package dj.eventregister.models.category;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Kategoria o tej samej nazwie już istnieje.")
+@ResponseStatus(value = HttpStatus.CONFLICT, reason = "This category name already exist.")
 public class DuplicateCategoryNameException extends RuntimeException {
 }
