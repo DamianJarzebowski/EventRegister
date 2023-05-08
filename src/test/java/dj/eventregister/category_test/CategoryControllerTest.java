40218d14af36d760f62b7c56c1b9f8f47ba63f63
Damian Jarzebowski <damian.r.hajduk@o2.pl>
2023-05-08 22:07:36 +0200

    clear import

--- src/test/java/dj/eventregister/category_test/CategoryControllerTest.java
+++ src/test/java/dj/eventregister/category_test/CategoryControllerTest.java
@@ -9,7 +9,6 @@ import org.assertj.core.api.Assertions;
 import org.assertj.core.internal.bytebuddy.utility.RandomString;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
